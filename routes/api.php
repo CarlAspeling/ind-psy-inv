5b3b5f057f60a964dd3d77a15c0444ec4d7c55ef
Aspeling94 <carlaspeling@gmail.com>
2025-05-05 12:58:45 +0200

    Added a new Route to handle post requests from questionnaire submission.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\QuestionnaireController;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/questionnaire', [QuestionnaireController::class, 'show']);
+
+Route::post('/save-response', [ResponseController::class, 'store']);
