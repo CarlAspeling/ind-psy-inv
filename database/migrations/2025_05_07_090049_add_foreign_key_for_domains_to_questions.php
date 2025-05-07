4c209da7d4b43cd25f307d2875d50c33bfe31d1e
Aspeling94 <carlaspeling@gmail.com>
2025-05-07 12:16:22 +0200

    Created migrations for the domains model, added a foreign key to map the relationship between it and questions, and removed the no longer necessary text-based domains column in the questions table.

---
+++ database/migrations/2025_05_07_090049_add_foreign_key_for_domains_to_questions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->foreignId('domain_id')
+                ->after('id')
+                ->nullable()
+                ->constrained('domains')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropForeign('questions_domain_id_foreign');
+            $table->dropColumn('domain_id');
+        });
+    }
+};
